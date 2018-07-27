1f8bf064442c24f9c08654136146a7aef165ae47
liuxudong2017 <liuxudong2017@sina.com>
2018-07-27 11:05:48 +0800

    2018-7-27 page one interface pay err 666

--- DYun/js/confirmOrder.js
+++ DYun/js/confirmOrder.js
@@ -93,19 +93,19 @@ var vm = new Vue({
 				console.log(res);
 				var data=res.data.data.xml;
 				var obj={
-					appId:data.appid,
-					device_info:data.device_info,
-					mch_id:data.mch_id,
-					nonceStr:data.nonce_str,
-					prepay_id:data.prepay_id,
-					result_code:data.result_code,
-					return_code:data.return_code,
-					return_msg:data.return_msg,
-					paySign:data.sign,
-					signType:"MD5",
-					package:"prepay_id="+data.prepay_id,
-					timeStamp:data.mch_id,
-					trade_type:data.trade_type
+					"appId":data.appid,
+					//device_info:data.device_info,
+					//mch_id:data.mch_id,
+					"nonceStr":data.nonce_str,
+					//prepay_id:data.prepay_id,
+					//result_code:data.result_code,
+					//return_code:data.return_code,
+					//return_msg:data.return_msg,
+					"paySign":data.sign,
+					"signType":"MD5",
+					"package":"prepay_id="+data.prepay_id,
+					"timeStamp":data.mch_id,
+					//trade_type:data.trade_type
 				}
 				callpay(obj);
 			});
